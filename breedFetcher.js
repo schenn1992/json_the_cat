2c0d4e81589180642e926d24f6c799f40cfd162c
Steven <schenn1992@gmail.com>
2020-11-05 18:25:01 +0000

    specify breed name using CLI arg

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,15 @@
 const request = require('request');
 
-request("https://api.thecatapi.com/v1/breeds/search?q=sib", (error, response, body) => {
+const breed = process.argv[2];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
   if (error) {
     console.log('fails to download: ', error, response.statusCode); 
     return;
   }
   
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(data[0]);
+  //console.log(data);
+  console.log(data[0]["description"]);
 
 });
