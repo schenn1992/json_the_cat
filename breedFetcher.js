c1438e014fa6d352c51f475e860c36320da33482
Steven <schenn1992@gmail.com>
2020-11-05 21:38:46 +0000

    conditional change

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,16 +3,13 @@ const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-    if (error) {
-      callback(error);
-    } else {
-      if (body === '[]') {
-        callback("Breed not Found");
-      } else {
-        const data = JSON.parse(body);
-        callback(data[0]["description"]);
-      }
-    }
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+      let errorMessage = "Breed not Found"
+      callback(errorMessage, null);
+  } else { 
+      callback(null, data[0]["description"]);
+  }
   });
 };
 
