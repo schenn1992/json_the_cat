40c6dacf46580ff636d86b624aab59520a963bc0
Steven <schenn1992@gmail.com>
2020-11-05 18:55:34 +0000

    print Breed not Found

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,14 +2,18 @@ const request = require('request');
 
 const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log('fails to download: ', error, response.statusCode); 
-    return;
+const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`
+
+request(URL, (error, response, body) => {
+  
+  if (body === '[]') {
+    console.log("Breed not Found");
+  } else {
+
+  const data = JSON.parse(body);
+  console.log(data[0]["description"]);
+  
   }
   
-  const data = JSON.parse(body);
-  //console.log(data);
-  console.log(data[0]["description"]);
 
 });
